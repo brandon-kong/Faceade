ff9720c214f034e866fefbed48b7469f53898abc
konginator <69498235+konginator@users.noreply.github.com>
2023-04-27 00:25:50 -0500

    c

--- src/pages/index.js
+++ src/pages/index.js
@@ -21,7 +21,9 @@ import { Inter, Albert_Sans } from 'next/font/google'
 const inter = Inter({ subsets: ['latin'] })
 
 fetch('/api/socket')
-const socket = io('https://faceade.io');
+const socket = io(PUBLIC_URL, {
+    transports: ['websocket'],
+});
 
 Socket.io = socket;
 
@@ -155,7 +157,9 @@ export default function Home() {
     useEffect(() => {
         if (Socket.io !== null) { Socket.io.close(); Socket.Game = null; }
         fetch('/api/socket')
-        const socket = io('https://faceade.io');
+        const socket = io(PUBLIC_URL, {
+            transports: ['websocket'],
+        });
 
         Socket.io = socket;
     }, [])
