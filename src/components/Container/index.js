441acc90b89f92ce868e52784cf19a58dd1d5f8b
konginator <69498235+konginator@users.noreply.github.com>
2023-04-26 22:45:51 -0500

    fixed production bugs

--- src/components/Container/index.js
+++ src/components/Container/index.js
@@ -1,6 +1,6 @@
 import styles from '@/styles/components/Container/index.module.css'
 
-export default ({children}) => {
+export default Container = ({children}) => {
     return (
         <div className={"flex items-center rounded justify-center bg-inherit h-12 w-full " + styles['container']} >
             {children}
