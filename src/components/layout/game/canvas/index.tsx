ebb0744cc8af52984c7893bcd67dab110181d097
brandon-kong <95515854+brandon-kong@users.noreply.github.com>
2023-12-31 17:29:18 -0600

    Added scalable canvas

--- src/components/layout/game/canvas/index.tsx
+++ src/components/layout/game/canvas/index.tsx
@@ -1,60 +1,127 @@
 'use client';
 
+import { useState, useCallback } from "react";
 import { useEffect } from "react";
+
+type DrawingAction = {
+    type: 'line';
+    x1: number;
+    y1: number;
+    x2: number;
+    y2: number;
+    radius: number;
+    color: string;
+}
 
 export default function Canvas ()
 {
-
+    const [drawingActions, setDrawingActions] = useState<DrawingAction[]>([]);
     // allow drawing on canvas
+    const [painting, setPainting] = useState<boolean>(false);
+    const [lastX, setLastX] = useState<number>(0);
+    const [lastY, setLastY] = useState<number>(0);
+
+    const draw = useCallback((e: MouseEvent) => {
+        if (!painting) return;
+
+        const canvas = document.getElementById('canvas') as HTMLCanvasElement;
+        const ctx = canvas.getContext('2d');
+        if (!ctx) return;
+
+        const rect = canvas.getBoundingClientRect();
+
+        const x = e.clientX - rect.left;
+        const y = e.clientY - rect.top;
+
+        ctx.lineWidth = 10;
+        ctx.lineCap = 'round';
+        ctx.lineTo(x, y);
+        ctx.stroke();
+        ctx.beginPath();
+        ctx.moveTo(x, y);
+
+        setDrawingActions(prev => [
+            ...prev,
+            {
+                type: 'line',
+                x1: lastX / canvas.width,
+                y1: lastY / canvas.height,
+                x2: (e.clientX - rect.left) / canvas.width,
+                y2: (e.clientY - rect.top) / canvas.height,
+                radius: 10,
+                color: 'black',
+            }
+        ]);      
+        
+        setLastX(x);
+        setLastY(y);
+    }
+    , [lastY, lastX, painting]);
+
+    const redraw = useCallback(() => {
+        const canvas = document.getElementById('canvas') as HTMLCanvasElement;
+        const ctx = canvas.getContext('2d');
+        if (!ctx) return;
+
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+        for (let action of drawingActions) {
+            if (action.type === 'line') {
+                ctx.beginPath();
+                ctx.lineWidth = action.radius * 2;
+                ctx.lineCap = 'round';
+                ctx.strokeStyle = action.color;
+                ctx.moveTo(action.x1 * canvas.width, action.y1 * canvas.height);
+                ctx.lineTo(action.x2 * canvas.width, action.y2 * canvas.height);
+                ctx.stroke();
+            }
+        }
+    }, [drawingActions]);
 
     useEffect(() => {
 
         const canvas = document.getElementById('canvas') as HTMLCanvasElement;
-        canvas.width = canvas.offsetWidth;
-        canvas.height = canvas.offsetHeight;
+        const dpr = window.devicePixelRatio || 1;
+        canvas.width = canvas.offsetWidth * dpr;
+        canvas.height = canvas.offsetHeight * dpr;
+        
 
         const ctx = canvas.getContext('2d');
-
-        let painting = false;
+        
+        ctx && ctx.scale(dpr, dpr);
 
         function startPosition(e: MouseEvent) {
-            painting = true;
+            setPainting(true);
             draw(e);
-        }
-
-        function finishedPosition() {
-            if (!ctx) return;
-            painting = false;
-            ctx.beginPath();
-        }
-
-        function draw(e: MouseEvent) {
-            if (!painting) return;
-            if (!ctx) return;
 
             const rect = canvas.getBoundingClientRect();
             const x = e.clientX - rect.left;
             const y = e.clientY - rect.top;
-            
-            ctx.lineWidth = 10;
-            ctx.lineCap = 'round';
-            ctx.lineTo(x, y);
-            ctx.stroke();
+
+            setLastX(x);
+            setLastY(y);
+        }
+
+        function finishedPosition() {
+            if (!ctx) return;
+            setPainting(false);
             ctx.beginPath();
-            ctx.moveTo(x, y);
         }
 
         canvas.addEventListener('mousedown', startPosition);
         canvas.addEventListener('mouseup', finishedPosition);
-        //canvas.addEventListener('mouseleave', finishedPosition)
 
         canvas.addEventListener('mousemove', draw);
         
 
-        const resizeCanvas = () => {
-            canvas.width = canvas.offsetWidth;
-            canvas.height = canvas.offsetHeight;
-        };
+        function resizeCanvas() {
+            const dpr = window.devicePixelRatio || 1;
+            canvas.width = canvas.offsetWidth * dpr;
+            canvas.height = canvas.offsetHeight * dpr;
+            ctx && ctx.scale(dpr, dpr);
+            
+            redraw();
+        }
         
         window.addEventListener('resize', resizeCanvas);
 
@@ -68,7 +135,7 @@ export default function Canvas ()
             canvas.removeEventListener('mousemove', draw);
             window.removeEventListener('resize', resizeCanvas);
         }
-    }, []);
+    }, [draw, redraw]);
 
     return (
         <div
